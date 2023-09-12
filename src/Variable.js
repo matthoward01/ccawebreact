da0aff84f1b2c85a7d574641c5616a4c8339f62f
Matt Howard <matthoward30740@gmail.com>
2023-09-12 08:28:19 -0400

    Change to api connection

--- src/Variable.js
+++ src/Variable.js
@@ -1,4 +1,6 @@
 export const variables = {
-  API_CCA: "https://localhost:44368/CCAContoller/",
-  API_CCASS: "https://localhost:44368/CCAContollerSS/",
+  API_CCA: "http://10.1.8.74:4000/CCAContoller/",
+  API_CCASS: "http://10.1.8.74:4000/CCAContollerSS/",
+  /* API_CCA: "https://localhost:44368/CCAContoller/",
+  API_CCASS: "https://localhost:44368/CCAContollerSS/", */
 };
