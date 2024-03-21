cf3d6a47dc47080e5f36c71ac21c0a9e4ccdb9c5
Matt Howard <matthoward30740@gmail.com>
2024-03-21 10:27:48 -0400

    Updating for local api

--- src/Variable.js
+++ src/Variable.js
@@ -1,6 +1,6 @@
 export const variables = {
-  API_CCA: "http://10.1.8.74:4000/CCAContoller/",
-  API_CCASS: "http://10.1.8.74:4000/CCAContollerSS/",
-  /* API_CCA: "https://localhost:44368/CCAContoller/",
-  API_CCASS: "https://localhost:44368/CCAContollerSS/", */
+  /* API_CCA: "http://10.20.11.23:4000/CCAContoller/",
+  API_CCASS: "http://10.20.11.23:4000/CCAContollerSS/", */
+  API_CCA: "http://localhost:4000/CCAContoller/",
+  API_CCASS: "http://localhost:4000/CCAContollerSS/",
 };
